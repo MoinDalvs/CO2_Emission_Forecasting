f7ff1afc18ef2a35d9f3cb2c64a7bc7f82208464
Moin_Dalvs <99672298+MoinDalvs@users.noreply.github.com>
2022-08-26 13:48:57 +0530

    progress_bar_speed_adjusted

--- Github_Deployment.py
+++ Github_Deployment.py
@@ -307,7 +307,7 @@ if nav == "Model Evaluation":
     my_bar = st.progress(0)
 
     for percent_complete in range(100):
-       time.sleep(0.08)
+       time.sleep(0.1)
        my_bar.progress(percent_complete + 1)
     
 
