b7c15df107ddf2630b62497d3998f510c668bad3
Moin_Dalvs <moindalvs@gmail.com>
2022-08-27 23:39:23 +0530

    project_by

--- Github_Deployment.py
+++ Github_Deployment.py
@@ -52,23 +52,6 @@ visual_1 = pd.read_csv('https://raw.githubusercontent.com/MoinDalvs/CO2_Emission
 st.sidebar.subheader("Select an ⭕Option below")
 nav = st.sidebar.radio("",['About',"Yearly Forecasting", "Monthly Forecasting","Time Series Analysis","Model Evaluation"])
 
-st.sidebar.markdown('## About Me\n\
-<table>\
-<tr>\
-<td>\
-     <img src="https://avatars.githubusercontent.com/u/99672298?v=4" width="180"/>\
-     \
-     moindalvs@gmail.com\
-\
-<p align="center">\
-<a href = "https://github.com/MoinDalvs"><img src = "http://www.iconninja.com/files/241/825/211/round-collaboration-social-github-code-circle-network-icon.svg" width="36" height = "36"/></a>\
-<a href = "https://twitter.com/DalvsHubot"><img src = "https://www.shareicon.net/download/2016/07/06/107115_media.svg" width="36" height="36"/></a>\
-<a href = "https://www.linkedin.com/in/moin-dalvi-277b0214a//"><img src = "http://www.iconninja.com/files/863/607/751/network-linkedin-social-connection-circular-circle-media-icon.svg" width="36" height="36"/></a>\
-</p>\
-</td>\
-</tr> \
-  </table>', unsafe_allow_html=True)
-
 if nav == 'About':
 
     my_bar = st.progress(0)
@@ -979,3 +962,20 @@ if nav == "Monthly Forecasting":
     with col2:
        col2.write(f"Interactive Line plot Forecasting for the next {months} Months")
        col2.plotly_chart(fig)
+
+st.sidebar.markdown('## Project By\n\
+<table>\
+<tr>\
+<td>\
+     <img src="https://avatars.githubusercontent.com/u/99672298?v=4" width="180"/>\
+     \
+     moindalvs@gmail.com\
+\
+<p align="center">\
+<a href = "https://github.com/MoinDalvs"><img src = "http://www.iconninja.com/files/241/825/211/round-collaboration-social-github-code-circle-network-icon.svg" width="36" height = "36"/></a>\
+<a href = "https://twitter.com/DalvsHubot"><img src = "https://www.shareicon.net/download/2016/07/06/107115_media.svg" width="36" height="36"/></a>\
+<a href = "https://www.linkedin.com/in/moin-dalvi-277b0214a//"><img src = "http://www.iconninja.com/files/863/607/751/network-linkedin-social-connection-circular-circle-media-icon.svg" width="36" height="36"/></a>\
+</p>\
+</td>\
+</tr> \
+  </table>', unsafe_allow_html=True)
