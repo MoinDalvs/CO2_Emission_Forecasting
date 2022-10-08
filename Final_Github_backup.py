f9a2d2e23505f11af4f9586f6427d68aa8e23dc3
Moin_Dalvs <moindalvs@gmail.com>
2022-10-08 15:49:39 +0530

    fast_reload

--- Final_Github_backup.py
+++ Final_Github_backup.py
@@ -447,7 +447,7 @@ if selected == "Model Evaluation":
     my_bar = st.progress(0)
 
     for percent_complete in range(100):
-       time.sleep(0.1)
+       time.sleep(0.01)
        my_bar.progress(percent_complete + 1)
     
 
